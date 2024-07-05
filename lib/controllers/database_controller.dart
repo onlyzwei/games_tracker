2682dc13996e4264ebfa2f2268f5ccb4a8714e49
miranda <mirandalucas@id.uff.br>
2024-07-05 20:28:59 -0300

    controllers att

---
+++ lib/controllers/database_controller.dart
@@ -0,0 +1,82 @@
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io' as io;
+
+class DatabaseController {
+  static final DatabaseController _instance = DatabaseController._internal();
+  static Database? _db;
+
+  factory DatabaseController() {
+    return _instance;
+  }
+
+  DatabaseController._internal();
+
+  Future<Database?> get db async {
+    _db ??= await _initDb();
+    return _db;
+  }
+
+  Future<Database?> _initDb() async {
+    sqfliteFfiInit();
+
+    var databaseFactory = databaseFactoryFfi;
+    final io.Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+    String path = p.join(appDocumentsDir.path, "games_tracker.db");
+
+    Database? db = await databaseFactory.openDatabase(
+      path,
+      options: OpenDatabaseOptions(
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute("""
+            CREATE TABLE user(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name VARCHAR NOT NULL,
+              email VARCHAR NOT NULL,
+              password VARCHAR NOT NULL
+            );
+          """);
+          await db.execute("""
+            CREATE TABLE genre(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              name VARCHAR NOT NULL
+            );
+          """);
+          await db.execute("""
+            CREATE TABLE game(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER NOT NULL,
+              name VARCHAR NOT NULL UNIQUE,
+              description TEXT NOT NULL,
+              release_date VARCHAR NOT NULL,
+              FOREIGN KEY(user_id) REFERENCES user(id)
+            );
+          """);
+          await db.execute("""
+            CREATE TABLE game_genre(
+              game_id INTEGER NOT NULL,
+              genre_id INTEGER NOT NULL,
+              FOREIGN KEY(game_id) REFERENCES game(id),
+              FOREIGN KEY(genre_id) REFERENCES genre(id)
+            );
+          """);
+          await db.execute("""
+            CREATE TABLE review(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              user_id INTEGER NOT NULL,
+              game_id INTEGER NOT NULL,
+              score REAL NOT NULL,
+              description TEXT NOT NULL,
+              date VARCHAR NOT NULL,
+              FOREIGN KEY(user_id) REFERENCES user(id),
+              FOREIGN KEY(game_id) REFERENCES game(id)
+            );
+          """);
+        }
+      )
+    );
+    return db;
+  }
+}
--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,13 +1,9 @@
 import 'package:games_tracker/models/user.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
-import 'dart:io' as io;
+import 'package:games_tracker/controllers/database_controller.dart';
 
 class UserController {
   static const String tableName = "user";
   static final UserController _userController = UserController._internal();
-  static Database? _db;
 
   factory UserController() {
     return _userController;
@@ -15,78 +11,32 @@ class UserController {
 
   UserController._internal();
 
-  Future<Database?> get db async {
-    /*if (_db == null) {
-      _db = initDb();
-    }
-    If é substituído pelo comando "??="
-    */
-
-    _db ??= await _initDb();
-
-    return _db;
-  }
-
-  Future<Database?> _initDb() async {
-    sqfliteFfiInit();
-
-    var databaseFactory = databaseFactoryFfi;
-    final io.Directory appDocumentsDir = await getApplicationDocumentsDirectory();
-    String path = p.join(appDocumentsDir.path, "databases", "user.db");
-
-    Database? db = await databaseFactory.openDatabase(
-      path,
-      options: OpenDatabaseOptions(
-        version: 1,
-        onCreate: (db, version) async {
-          String sql = """
-          CREATE TABLE user(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR NOT NULL,
-            email VARCHAR NOT NULL,
-            password VARCHAR NOT NULL
-          );
-          """;
-          await db.execute(sql);
-        }
-      )
-    );
-    return db;  
-  }
-
   Future<int> insertUser(User user) async {
-    var database = await db;
-
-    int id = await database!.insert(tableName, user.toMap());
-
+    var db = await DatabaseController().db;
+    int id = await db!.insert(tableName, user.toMap());
     return id;
   }
 
-  getUsers() async {
-    var database = await db;
-    String sql = "SELECT * FROM $tableName";
-
-    List users = await database!.rawQuery(sql);
-
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    var db = await DatabaseController().db;
+    List<Map<String, dynamic>> users = await db!.query(tableName);
     return users;
   }
 
   Future<int> updateUser(User user) async {
-    var database = await db;
-
-    int result = await database!.update(
+    var db = await DatabaseController().db;
+    int result = await db!.update(
       tableName,
       user.toMap(),
       where: "id = ?",
-      whereArgs: [user.id!]
+      whereArgs: [user.id]
     );
     return result;
   }
 
   Future<int> deleteUser(int id) async {
-    var database = await db;
-
-    int result = await database!.delete(
+    var db = await DatabaseController().db;
+    int result = await db!.delete(
       tableName,
       where: "id = ?",
       whereArgs: [id]
